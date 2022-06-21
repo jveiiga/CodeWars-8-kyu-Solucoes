740d82a6da49e16dcfeb8a664065e0394c56fae1
Jeferson Veiga <jeferson.veiiga@icloud.com>
2022-06-21 13:45:02 -0300

    Atualizando BÁSICO: Fazendo Seis Torradas.js

---
+++ BÁSICO: Fazendo Seis Torradas.js
@@ -0,0 +1,15 @@
+function sixToast(num) {
+ 
+    if( num <= 6 ) {
+
+        return 6 - num
+
+    }else if( num > 6 ){
+
+        return num - 6
+
+    }
+}
+console.log(sixToast(6))
+console.log(sixToast(17))
+console.log(sixToast(3))
