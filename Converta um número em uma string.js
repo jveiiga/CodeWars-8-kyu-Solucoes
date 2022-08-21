737d2f17ee28f404cb02e64ac6a7c6d40ef0c728
Jeferson Veiga <jeferson.veiiga@icloud.com>
2022-08-21 18:42:30 -0300

    updating Converta um número em uma string!

---
+++ Converta um número em uma string.js
@@ -0,0 +1,9 @@
+function numberToString(num) {
+    return num.toString()
+}
+
+function numberToString2(num) {
+    return `${num}`
+}
+console.log(numberToString(67))
+console.log(numberToString2(67))
